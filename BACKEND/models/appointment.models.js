e39604a2885825d754d7b5dacd923babe8b6bb8c
anusha0507 <anushasahu0507@gmail.com>
2024-12-24 22:18:07 +0530

    Add more schemas

--- BACKEND/models/appointment.models.js
+++ BACKEND/models/appointment.models.js
@@ -1,8 +1,30 @@
 import mongoose from "mongoose";
 
-const appointmentSchema = mongoose.Schema(
-    {
-        appointment_id: String,
-        required: true,
-    }
-)
+const appointmentSchema = mongoose.Schema({
+  appointment_id: {
+    type: String,
+    required: true,
+  },
+  patient_id:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+  },
+  doctor_id:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+  },
+  appointment_date:{
+    type: String,
+    required: true
+  },
+  appointment_time:{
+    type: String,
+    required: true
+  },
+  status:{
+    type: String,
+    enum:["PENDING, CONFIRMED, CANCELLED"],
+    required: true
+  },
+
+});
